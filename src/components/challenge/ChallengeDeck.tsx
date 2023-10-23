42fb3154539b97b780174f656b760e6488495615
Matt Alton <mattalton97@gmail.com>
2023-10-23 20:08:03 +0100

    Added hover effect to challenge deck selection

--- src/components/challenge/ChallengeDeck.tsx
+++ src/components/challenge/ChallengeDeck.tsx
@@ -10,13 +10,14 @@ export default function ChallengeDeck({ deck }: Props) {
     <div className="flex justify-center">
       {deck.map((kanji) => {
         return (
-          <KanjiCard
-            key={kanji.id}
-            card={kanji}
-            onClick={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
+          <div className="-mx-2 hover:z-10 hover:-translate-y-1" key={kanji.id}>
+            <KanjiCard
+              card={kanji}
+              onClick={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          </div>
         );
       })}
     </div>
